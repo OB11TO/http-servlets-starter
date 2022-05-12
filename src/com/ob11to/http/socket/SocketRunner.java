000ddd4059b3a37c7f74f57d0a9ccbbaa128a337
OB11TO <OB11TO61@yandex.ru>
2022-05-12 12:56:58 +0300

    HTTP. Servlets. 9. TCP. Класс ServerSocket Цикл Send and Response

--- src/com/ob11to/http/socket/SocketRunner.java
+++ src/com/ob11to/http/socket/SocketRunner.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.Inet4Address;
 
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
     public static void main(String[] args) throws IOException {
@@ -14,14 +15,20 @@ public class SocketRunner {
 //        tcp
         var inetAddresses = Inet4Address.getByName("localhost");  //создание IP
         try (var socket = new Socket(inetAddresses, 7777);  // сокет (IP address, порт)
-            var outputStream = new DataOutputStream(socket.getOutputStream()); //создает поток ввода
-            var inputStream = new DataInputStream(socket.getInputStream())) { //создает поток вывода
+             var outputStream = new DataOutputStream(socket.getOutputStream()); //создает поток ввода
+             var inputStream = new DataInputStream(socket.getInputStream());
+             var scanner = new Scanner(System.in)) { //создает поток вывода
 
-            outputStream.writeUTF("Hello! I am Client"); //Записывает строку в базовый выходной поток
-           // var response = inputStream.readAllBytes(); //Считывает все оставшиеся байты из входного потока.
-            var readUTF = inputStream.readUTF();
-            System.out.println(readUTF);
-
+            while (scanner.hasNextLine()) {
+                var send = scanner.nextLine();
+                outputStream.writeUTF(send);
+                var responseServer = inputStream.readUTF();
+                if("stop".equals(responseServer)){
+                    break;
+                }else {
+                    System.out.println("Server: " + responseServer);
+                }
+            }
         }
 
     }
--- src/com/ob11to/http/socket/SocketServerRunner.java
+++ src/com/ob11to/http/socket/SocketServerRunner.java
@@ -3,18 +3,24 @@ package com.ob11to.http.socket;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketServerRunner {
     public static void main(String[] args) throws IOException {
         try (var serverSocket = new ServerSocket(7777);
              var socket = serverSocket.accept();
              var outputStream = new DataOutputStream(socket.getOutputStream());
-             var inputStream = new DataInputStream(socket.getInputStream())) {
+             var inputStream = new DataInputStream(socket.getInputStream());
+             var scanner = new Scanner(System.in)) {
 
-            var readUTF = inputStream.readUTF();
-            outputStream.writeUTF("Hello! I am Server");
-            System.out.println(readUTF);
-
+            var readSendClient = inputStream.readUTF();
+            while (!"stop".equals(readSendClient)) {
+                System.out.println("Client: " + readSendClient);
+                var responseServer = scanner.nextLine();
+                outputStream.writeUTF(responseServer);
+                readSendClient = inputStream.readUTF();
+            }
         }
     }
 }
+
