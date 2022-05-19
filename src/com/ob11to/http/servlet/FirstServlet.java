30b4112921790519148c68472ee997a847e54382
OB11TO <OB11TO61@yandex.ru>
2022-05-19 18:20:13 +0300

    HTTP. Servlets. 30. Работа с телом запроса. HTTP Body

--- src/com/ob11to/http/servlet/FirstServlet.java
+++ src/com/ob11to/http/servlet/FirstServlet.java
@@ -7,10 +7,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Stream;
 
 
-@WebServlet("/")
+@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
 
     @Override
@@ -30,12 +33,28 @@ public class FirstServlet extends HttpServlet {
             System.out.println(req.getHeader(header));
             System.out.println();
         }
+        var paramValue = req.getParameter("param");
+        var parameterMap = req.getParameterMap();
+        System.out.println();
+
+
         //RESPONSE
         resp.setContentType("text/html; charset=UTF-8"); //тип отправления клиенту | добавили русификацию
         resp.setHeader("token","123321"); // кастомный хедер
         //resp.setCharacterEncoding(StandardCharsets.UTF_8.name());  можно передавать, чтобы не было проблем с русификацией
         try (var writer = resp.getWriter()) { //поток вывода
             writer.write("<h1>ПРИВЕТ<h1/>"); //отправка
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var parameterMap = req.getParameterMap();
+        System.out.println(parameterMap);
+
+        try (var reader = req.getReader(); //поток считывания, чтобы считать запрос
+            var lines = reader.lines()){ //стрим строк из потока, который считали
+            lines.forEach(System.out::println);
         }
     }
 
