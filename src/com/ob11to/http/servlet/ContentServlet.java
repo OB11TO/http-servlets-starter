32f89d123b00f7d653ea0dd8d511852005508c36
OB11TO <OB11TO61@yandex.ru>
2022-05-25 15:04:49 +0300

    HTTP. Servlets. 42. Expression Language. EL

--- src/com/ob11to/http/servlet/ContentServlet.java
+++ src/com/ob11to/http/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.ob11to.http.servlet;
 
+import com.ob11to.http.dto.FlightDto;
+import com.ob11to.http.service.FlightService;
 import com.ob11to.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,11 +10,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
+
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var flightDtos = flightService.findAll(); //получаем список всех перелетов dto
+        req.setAttribute("flights", flightDtos); //задаем атрибуты (requestScope)
+        //задаем атрибуты (sessionScope)
+        req.getSession().setAttribute("flightsMap", flightDtos.stream().
+                collect(Collectors.toMap(FlightDto::getId,FlightDto::getDescription)));
+
+        //перенаправляем запрос в jsp
         req.getRequestDispatcher(JspHelper.getPath("content"))
                 .forward(req,resp);
     }
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -4,16 +4,29 @@
   Date: 25.05.2022
   Time: 12:44
   To change this template use File | Settings | File Templates.
+
+  Тэги:
+  <span> - для текста
+  <p> - абзац
+
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-
     <title>Content ob11to</title>
 </head>
 <body>
     <%@include file="header.jsp" %>
-    <h1>Content</h1>
+    <span>Content</span>
+    <p>Size: ${requestScope.flights.size()}</p>  <%-- размер --%>
+    <p>Id req: ${requestScope.flights[1].id}</p> <%--  --%>
+    <p>Id session: ${sessionScope.flightsMap[0]}</p>
+
+    <p>JSESSIONID: ${cookie["JSESSIONID"]}</p>  <%-- индентификатор сесии --%>
+    <p>Header: ${header["cookie"]}</p>  <%-- JSESSIONID всю строку куки --%>
+    <p>Param id: ${param.id}</p>
+    <p>Param test: ${param.test}</p>
+    <p>Empty list: ${requestScope.flights}</p>
     <%@include file="footer.jsp" %>
 </body>
 </html>
