46011f57e2c5774ad4fc88bb05b90021903bbdd7
OB11TO <OB11TO61@yandex.ru>
2022-05-30 18:06:32 +0300

    HTTP. Servlets. 53. Authorization. Авторизация

---
+++ src/com/ob11to/http/filter/AuthorizationFilter.java
@@ -0,0 +1,42 @@
+package com.ob11to.http.filter;
+
+import com.ob11to.http.dto.UserDto;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static com.ob11to.http.util.UrlPath.*;
+
+@WebFilter("/*")
+public class AuthorizationFilter implements Filter {
+
+    private static final Set<String> PUBLIC_PATH = Set.of(LOGIN, REGISTRATION, IMAGES); //сделал публичные пути, которые доступны
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        var requestURI = ((HttpServletRequest) servletRequest).getRequestURI();
+        if(isPublicPath(requestURI) || isUserLoggedId(servletRequest)){ //проверка на публичный путь и на зарегистрированного пользователя
+            filterChain.doFilter(servletRequest,servletResponse);
+        }else {
+            reject((HttpServletRequest) servletRequest, (HttpServletResponse) servletResponse); //отказ
+        }
+    }
+
+    private void reject(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws IOException {
+        var prevPage = servletRequest.getHeader("referer"); //предыдущая страница
+        servletResponse.sendRedirect(prevPage != null ? prevPage : LOGIN); //Возвращаемся на пред страницу иначе на логин
+    }
+
+    private boolean isUserLoggedId(ServletRequest servletRequest) {
+        var user = (UserDto) ((HttpServletRequest) servletRequest).getSession().getAttribute("user");
+        return user != null;
+    }
+
+    private boolean isPublicPath(String requestURI) {
+        return PUBLIC_PATH.stream().anyMatch(requestURI::startsWith);
+    }
+}
--- src/com/ob11to/http/servlet/LogoutServlet.java
+++ src/com/ob11to/http/servlet/LogoutServlet.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 
+
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getSession().invalidate();
